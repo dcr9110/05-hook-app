99bab9d05c736c1de38b9407289a6de2f933cd50
Dcano <diegocn217@gmail.com>
2020-09-22 00:52:48 -0300

    Se realizo un formulario con useEffect y luego con se lo sustituyo con un custon hook .useForm

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -4,15 +4,16 @@ import './counter.css';
 
 export default function CounterWithCustomHook() {
     
-    const {state, increment, decrement, reset} =  useCounter(24);
-    
+    const {state, increment, decrement, reset} =  useCounter(50);
+    const classBtn = "btn btn-primary";
+
     return (
         <>
             <h1>Counter with Hook: {state}</h1>
             <hr/>
-            <button className="btn btn-primary" onClick={()=>{increment(2)}}> +1 </button>
-            <button className="btn btn-primary" onClick={reset}> reset </button>
-            <button className="btn btn-primary" onClick={()=>{decrement(2)}}> -1 </button> 
+            <button className={classBtn} onClick={()=>{increment(2)}}> +1 </button>
+            <button className={classBtn} onClick={reset}> reset </button>
+            <button className={classBtn} onClick={()=>{decrement(2)}}> -1 </button> 
         </>
     )
 }
---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import './effects.css';
+import { Message } from './Message';
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: ''
+    });
+    const {name, email} = formState;
+
+    useEffect(()=>{
+        console.log("se creo el objeto")
+    },[]);
+
+    useEffect(()=>{
+        console.log("el formulario cambio")
+    },[formState]);
+
+    useEffect(()=>{
+        console.log("cambio el email")
+    },[email]);
+
+    const handleInputChange= ({target}) =>{
+        
+        setFormState({
+            ...formState,
+            [target.name]: target.value,
+        })
+
+    };
+
+    return (
+        <>
+            <h1>useEffect</h1>
+            <hr/>
+
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Tu nombre"
+                    autoComplete="off"
+                    value={name}
+                    onChange={ handleInputChange }
+                />
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="email@gmail.com"
+                    autoComplete="off"
+                    value={email}
+                    onChange={ handleInputChange }
+                />
+            </div>
+
+            { ( name === '123' ) && <Message/>}
+        </>
+    )
+}
